e7d142fb11568bc5647b0b3386c38f9c241b1957
Sanowarsunny <85189283+Sanowarsunny@users.noreply.github.com>
2021-10-31 14:07:32 +0600

    update

--- bloodhub/volunteer.php
+++ bloodhub/volunteer.php
@@ -4,7 +4,9 @@
 <head>
 	
 	<title>volunteer</title>
-	<link rel="stylesheet"  href="style/volunteer.css">
+	<link rel="stylesheet"  href="style/volunteers.css">
+	  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
 </head>
 <body>
 	<header>
@@ -65,11 +67,32 @@
 		it is to roll up a sleeve and help save lives.
 		</p>	
     </div>
+	<footer class="footer">
+  	 <div class="container1">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>Blood Hub</h4>
+  	 			<ul>
+  	 				<li><a href="about.php">about us</a></li>
+  	 				<li><a href="#">our services</a></li>
+  	 				<li><a href="#">privacy policy</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		
+  	 		
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
 	
-	<div class="div-bottom">
-		
-		
-	</div>
 	
 	
 </body>
